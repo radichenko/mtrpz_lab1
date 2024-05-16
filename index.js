bdb87b1a6a1c34357d588bcf34f725c6db70e4c3
radichenko <115370873+radichenko@users.noreply.github.com>
2024-05-16 14:37:04 +0300

    Implemented the 'readSource' function

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const { checkMarkers, checkNesting } = require('./markdownProcessor');
 const convertToHtml = require('./htmlConverter');
 
+const source = process.argv[2];
+const destination = process.argv[4];
+
+if (!source) {
+    console.error('Error: No source specified');
+    process.exit(1);
+}
+
+const readSource = (input, destination, processContent) => {
+    fs.readFile(input, 'utf8', (err, data) => {
+        if (err) {
+            console.error(`Error reading from: ${input}`, err);
+            process.exit(1);
+        }
+
+        processContent(data, destination); 
+    });
+};
+
 function processContent(data, output) {
 
 }
