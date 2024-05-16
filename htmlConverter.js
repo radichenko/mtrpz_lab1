852851156cb3d9914e713aa15701b70613e13cdc
radichenko <115370873+radichenko@users.noreply.github.com>
2024-05-16 14:58:32 +0300

    Created the 'htmlConverter' function

--- htmlConverter.js
+++ htmlConverter.js
@@ -1,5 +1,36 @@
 const htmlConverter = (data) => {
+    let html = '';
+    const lines = data.split('\n');
+    let inPre = false;
+    let newParagraph = true;
 
-}
+    lines.forEach((line, index) => {
+        if (line.startsWith('```')) {
+            inPre = !inPre;
+            html += inPre ? '<pre>' : '</pre>\n';
+            newParagraph = true;
+        } else if (inPre) {
+            html += `${line}\n`;
+        } else {
+            if (line.trim() !== '') {
+                if (newParagraph && html !== '') {
+                    html += '<p>';
+                }
+                newParagraph = false;
+
+                line = line.replace(/\*\*(.*?)\*\*/g, '<b>$1</b>')
+                    .replace(/_(.*?)_/g, '<i>$1</i>')
+                    .replace(/`(.*?)`/g, '<code>$1</code>');
+
+                html += line;
+                if ((lines[index + 1] && lines[index + 1].trim() === '') || index === lines.length - 1) {
+                    html += '</p>\n';
+                    newParagraph = true;
+                }
+            }
+        }
+    });
+    return html;
+};
 
 module.exports = htmlConverter;
