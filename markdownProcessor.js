767d01456717e7adbe56a18d788201f47678d6c1
radichenko <115370873+radichenko@users.noreply.github.com>
2024-05-16 15:08:40 +0300

    Added function to check the markers

--- markdownProcessor.js
+++ markdownProcessor.js
@@ -1,8 +1,39 @@
 const { htmlConverter } = require('./htmlConverter');
 
 const checkMarkers = (data) => {
+    let bold = false;
+    let monospace = false;
+    let italic = false;
+    let preformatted = 0;
 
-}
+    for (let i = 0; i < data.length; i++) {
+        if (data[i] === '*' && i + 1 < data.length && data[i + 1] === '*') {
+            if (!(i > 0 && data[i - 1] === '\\')) {
+                bold = !bold;
+                i++;
+            }
+        } else if (data[i] === '`' && (i < 1 || data[i - 1] !== '`')) {
+            if (!(i > 0 && data[i - 1] === '\\')) {
+                monospace = !monospace;
+            }
+        } else if (data[i] === '_') {
+            if (!(i > 0 && data[i - 1] === '\\')) {
+                const hasSpace = (i > 0 && data[i - 1] === ' ') || (i < data.length - 1 && data[i + 1] === ' ');
+                const hasQuote = (i > 0 && data[i - 1] === '‘') || (i < data.length - 1 && data[i + 1] === '’');
+                const partOfWord = (i > 0 && data[i - 1].match(/\w/)) && (i < data.length - 1 && data[i + 1].match(/\w/));
+
+                if (!hasSpace && !hasQuote && !partOfWord) {
+                    italic = !italic;
+                }
+            }
+        } else if (data[i] === '`' && i + 2 < data.length && data[i + 1] === '`' && data[i + 2] === '`') {
+            preformatted++;
+            i += 2;
+        }
+    }
+
+    return !(bold || monospace || italic || preformatted % 2 !== 0);
+};
 
 const checkNesting = (data) => {
 
