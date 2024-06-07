7581a201ebad0d7f97361ac2961c43d82dc13f67
radichenko <radichenkonazar@gmail.com>
2024-06-07 17:55:03 +0300

    no more space replacing!

--- htmlConverter.js
+++ htmlConverter.js
@@ -20,9 +20,9 @@ const htmlConverter = (data) => {
                     newLineCount = 0;
                 }
 
-                line = line.replace(/(?:\s|^)\*\*(\S.*?\S|\S)\*\*(?:\s|$)/g, '<b>$1</b>')
-                    .replace(/(?:\s|^)_(\S.*?\S|\S)_(?:\s|$)/g, '<i>$1</i>')
-                    .replace(/(?:\s|^)`(\S.*?\S|\S)`(?:\s|$)/g, '<code>$1</code>');
+                line = line.replace(/(\s|^)\*\*(\S.*?\S|\S)\*\*(\s|$)/g, '$1<b>$2</b>$3')
+                    .replace(/(\s|^)_(\S.*?\S|\S)_(\s|$)/g, '$1<i>$2</i>$3')
+                    .replace(/(\s|^)`(\S.*?\S|\S)`(\s|$)/g, '$1<tt>$2</tt>$3');
 
                 html += line + '\n';
             } else {
